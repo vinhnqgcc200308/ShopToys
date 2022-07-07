4083d5fa7899956af3952b14a7917086dd0755f0
datnhism <98210435+datnhism@users.noreply.github.com>
2022-07-07 23:26:08 +0700

    Update AboutController.php

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -6,15 +6,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+// class AboutController extends AbstractController
+// {
+//     /**
+//      * @Route("/about", name="about_page")
+//      */
+//     public function aboutAction(): Response
+//     {
+//         return $this->render('about/index.html.twig', [
+//             'controller_name' => 'AboutController',
+//         ]);
+//     }
+// }
 class AboutController extends AbstractController
 {
     /**
      * @Route("/about", name="about_page")
-     */
+      */
     public function aboutAction(): Response
-    {
-        return $this->render('about/index.html.twig', [
+     {
+         return $this->render('about/index.html.twig', [
             'controller_name' => 'AboutController',
-        ]);
-    }
+         ]);
+     }
 }
