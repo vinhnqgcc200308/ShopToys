5556d8832199497515b18d3d440004f861a87936
vinhnqgcc200308 <100894619+vinhnqgcc200308@users.noreply.github.com>
2022-07-07 19:40:39 +0700

    Create CustomerController.php
    Create CustomerController.php
    
    Co-Authored-By: vinhnguyen200308 <100894619+vinhnqgcc200308@users.noreply.github.com>

---
+++ src/Controller/CustomersController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CustomersRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+date_default_timezone_set("Asia/Ho_Chi_Minh");
+class CustomersController extends AbstractController
+{
+    // #[Route('/customers', name: 'app_customers')]
+    // public function index(): Response
+    // {
+    //     return $this->render('customers/index.html.twig', [
+    //         'controller_name' => 'CustomersController',
+    //     ]);
+    // }
+
+    /**
+     * @Route("customers", name="app_customers")
+     */
+    public function customersAction(CustomersRepository $repo): Response
+    {
+        $customers = $repo->findAll();
+        return $this->render('customers/index.html.twig', [
+            'customers' => $customers
+        ]);
+    }
+}
