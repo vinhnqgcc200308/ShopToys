1d93b46cb61ae9fb25a2cb9d3a9f0f1ffec4bfdf
vinhnqgcc200308 <100894619+vinhnqgcc200308@users.noreply.github.com>
2022-07-07 19:44:59 +0700

    Create Orders.php
    Create Orders.php
    
    Co-Authored-By: vinhnguyen200308 <100894619+vinhnqgcc200308@users.noreply.github.com>

---
+++ src/Entity/Orders.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersRepository::class)]
+class Orders
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\ManyToOne(targetEntity: Customers::class, inversedBy: 'orders')]
+    private $customers;
+
+    #[ORM\Column(type: 'datetime')]
+    private $Date;
+
+    #[ORM\Column(type: 'datetime')]
+    private $Delivery;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Status;
+
+    #[ORM\OneToMany(mappedBy: 'orders', targetEntity: Orderdetails::class)]
+    private $orderdetails;
+
+    public function __construct()
+    {
+        $this->orderdetails = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomers(): ?Customers
+    {
+        return $this->customers;
+    }
+
+    public function setCustomers(?Customers $customers): self
+    {
+        $this->customers = $customers;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getDelivery(): ?\DateTimeInterface
+    {
+        return $this->Delivery;
+    }
+
+    public function setDelivery(\DateTimeInterface $Delivery): self
+    {
+        $this->Delivery = $Delivery;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->Status;
+    }
+
+    public function setStatus(string $Status): self
+    {
+        $this->Status = $Status;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Orderdetails>
+     */
+    public function getOrderdetails(): Collection
+    {
+        return $this->orderdetails;
+    }
+
+    public function addOrderdetail(Orderdetails $orderdetail): self
+    {
+        if (!$this->orderdetails->contains($orderdetail)) {
+            $this->orderdetails[] = $orderdetail;
+            $orderdetail->setOrders($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrderdetail(Orderdetails $orderdetail): self
+    {
+        if ($this->orderdetails->removeElement($orderdetail)) {
+            // set the owning side to null (unless already changed)
+            if ($orderdetail->getOrders() === $this) {
+                $orderdetail->setOrders(null);
+            }
+        }
+
+        return $this;
+    }
+}
