373dc3e9e07846880aafa72bd5e847cf543c2042
Tuan998 <100896109+Tuan998@users.noreply.github.com>
2022-07-08 00:18:31 +0700

    Create app.js
    Create app.js

---
+++ public/app.js
@@ -0,0 +1,25 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 25) {
+        header.style.backgroundColor = '#fa2a00';
+    } else {
+        header.style.backgroundColor = '#fb5533';
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
