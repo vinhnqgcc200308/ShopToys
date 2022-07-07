10e7cb81736a0433dbc081df0978c8de503d9279
vinhnqgcc200308 <100894619+vinhnqgcc200308@users.noreply.github.com>
2022-07-07 19:48:15 +0700

    Create Supplier.php
    Create Supplier.php
    
    Co-Authored-By: vinhnguyen200308 <100894619+vinhnqgcc200308@users.noreply.github.com>

---
+++ src/Entity/Suppliers.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SuppliersRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SuppliersRepository::class)]
+class Suppliers
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $Nation;
+
+    #[ORM\OneToMany(mappedBy: 'suppliers', targetEntity: Products::class)]
+    private $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNation(): ?string
+    {
+        return $this->Nation;
+    }
+
+    public function setNation(string $Nation): self
+    {
+        $this->Nation = $Nation;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Products>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProduct(Products $product): self
+    {
+        if (!$this->products->contains($product)) {
+            $this->products[] = $product;
+            $product->setSuppliers($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Products $product): self
+    {
+        if ($this->products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getSuppliers() === $this) {
+                $product->setSuppliers(null);
+            }
+        }
+
+        return $this;
+    }
+}
