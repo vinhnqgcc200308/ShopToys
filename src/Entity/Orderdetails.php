551dea24a5ba1c6e0a929b3a0e5e2b200da7eef5
vinhnqgcc200308 <100894619+vinhnqgcc200308@users.noreply.github.com>
2022-07-07 19:44:09 +0700

    Create Orderdetails.php
    Create Orderdetails.php
    
    Co-Authored-By: vinhnguyen200308 <100894619+vinhnqgcc200308@users.noreply.github.com>

---
+++ src/Entity/Orderdetails.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderdetailsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrderdetailsRepository::class)]
+class Orderdetails
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $quantity;
+
+    #[ORM\ManyToOne(targetEntity: Orders::class, inversedBy: 'orderdetails')]
+    private $orders;
+
+    #[ORM\ManyToOne(targetEntity: Products::class, inversedBy: 'orderdetails')]
+    private $products;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): self
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getOrders(): ?Orders
+    {
+        return $this->orders;
+    }
+
+    public function setOrders(?Orders $orders): self
+    {
+        $this->orders = $orders;
+
+        return $this;
+    }
+
+    public function getProducts(): ?Products
+    {
+        return $this->products;
+    }
+
+    public function setProducts(?Products $products): self
+    {
+        $this->products = $products;
+
+        return $this;
+    }
+}
